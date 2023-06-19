70f5c6f1f8ab822acc38007dcab977b16ba1b7f4
Gamilliam <BRNLCECILE@GMAIL.COM>
2023-06-19 11:20:49 +0200

    Done

--- userHandlers.js
+++ userHandlers.js
@@ -29,7 +29,21 @@ const getUserById = (req, res) => {
         });
 };
 
+const postUser = (req, res) => {
+    const {firstname, lastname, email, city, language} = req.body;
+    database
+    .query(`INSERT INTO users (firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)`
+        ,[firstname, lastname, email, city, language])
+    .then(([result]) => {
+        res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+        res.status(500).send("Error inserting user into database");
+    });
+}
+
 module.exports = {
     getUsers,
     getUserById,
+    postUser
 };
