1f848f0db52b636af856b47580e228bde2ff5070
Gamilliam <BRNLCECILE@GMAIL.COM>
2023-06-19 16:15:01 +0200

    Validators done but forgot to create PUT routes

--- validators.js
+++ validators.js
@@ -1,27 +1,29 @@
+const Joi = require('joi');
+
 // ----------------------------------------------------------------
 // MOVIES
+// title varchar(255) NOT NULL,
+// director varchar(255) NOT NULL,
+// year varchar(255) NOT NULL,
+// color varchar(255) NOT NULL,
+// duration int NOT NULL
+const movieSchema = Joi.object({
+  title : Joi.string().max(255).required(),
+  director : Joi.string().max(255).required(),
+  year : Joi.string().max(255).required(),
+  color : Joi.boolean().required(),
+  duration : Joi.number().integer().required(),
+});
+
 const validateMovie = (req, res, next) => {
   const { title, director, year, color, duration } = req.body;
-  const errors = [];
+  const {error} = movieSchema.validate(
+    {title, director, year, color, duration},
+    {abortEarly: false}
+    );
 
-  if (title == null && title.length > 255) {
-    errors.push({ field: "title", message: "This field is required and should contain less than 255 characters"});
-  }
-  if (director == null && director.length > 255) {
-    errors.push({ field: "director", message: "This field is required and should contain less than 255 characters"});
-  }
-  if (year == null && year.length > 255) {
-    errors.push({ field: "year", message: "This field is required and should contain less than 255 characters"});
-  }
-  if (color == null && color.length > 255) {
-    errors.push({ field: "color", message: "This field is required and should contain less than 255 characters"});
-  }
-  if (duration == null && typeof duration === "int") {
-    errors.push({ field: "duration", message: "This field is required" });
-  }
-
-  if (errors.length) {
-    res.status(422).json({ validationErrors: errors });
+  if (error) {
+    res.status(422).json({ validationErrors: error.details });
   } else {
     next();
   }
@@ -30,33 +32,32 @@ const validateMovie = (req, res, next) => {
 
 // ----------------------------------------------------------------
 // USERS
+// firstname varchar(255) NOT NULL,
+// lastname varchar(255) NOT NULL,
+// email varchar(255) UNIQUE NOT NULL,
+// city varchar(255) DEFAULT NULL,
+// language varchar(255) DEFAULT NULL
+
+const userSchema = Joi.object({
+  firstname : Joi.string().max(255).required(),
+  lastname : Joi.string().max(255).required(),
+  email : Joi.string().max(255).required(),
+  city : Joi.string().max(255),
+  language : Joi.string().max(255),
+});
+
 const validateUser = (req, res, next) => {
   const { firstname, lastname, email, city, language } = req.body;
-  const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
-  const errors = [];
+  const { error } = userSchema.validate(
+    {firstname, lastname, email, city, language},
+    {abortEarly: false}
+    )
 
-  if (firstname == null && firstname.length > 255) {
-    errors.push({ field: "firstname", message: "This field is required and should contain less than 255 characters"});
-  }
-  if (lastname == null && lastname.length > 255) {
-    errors.push({ field: "lastname", message: "This field is required and should contain less than 255 characters"});
-  }
-  if (email == null && email.length > 255 && !emailRegex.test(email)) {
-    errors.push({ field: "email", message: "This field is required and with format 'exmaple@syrup.com' and should contain less than 255 characters"});
-  }
-  if (city.length > 255) {
-    errors.push({ field: "city", message: "This field should contain less than 255 characters"});
-  }
-  if (language.length > 255) {
-    errors.push({ field: "language", message: "This field should contain less than 255 characters" });
-  }
-
-  if (errors.length) {
-    res.status(422).json({ validationErrors: errors });
+  if (error) {
+    res.status(422).json({ validationErrors: error.details });
   } else {
     next();
-  }
-};
+  }};
 
 module.exports = {
   validateMovie,
